7bba704b75ef7eb6df8e472d7789d82a0ee3f271
Bryium <bryiumonyancha@gmail.com>
2025-06-15 21:30:10 +0300

    Remove interface.py from tracking and add to .gitignore

--- interface.py
+++
@@ -1,7 +0,0 @@
-from scapy.all import get_if_list, get_if_hwaddr
-
-for iface in get_if_list():
-    try:
-        print(f"{iface} - {get_if_hwaddr(iface)}")
-    except:
-        print(f"{iface} - Unable to get MAC")
